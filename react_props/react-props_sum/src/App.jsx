e67f80b22a0378485a6f4b44ae0f706f0e4647a0
Felix <Fatkidkiter@gmail.com>
2025-04-22 15:21:28 +0200

    task 2 - calculating the sum - solved

---
+++ react_props/react-props_sum/src/App.jsx
@@ -0,0 +1,9 @@
+
+function Sum({ valueA, valueB }) {
+  const sum = valueA + valueB;
+  return <h1>{valueA} + {valueB} = {sum}</h1>;
+}
+
+export default function App() {
+  return <Sum valueA={5} valueB={3} />;
+}
