efccabcaf767bbbae796165fb89fff608446b2b1
Felix <Fatkidkiter@gmail.com>
2025-04-24 12:27:03 +0200

    first task - fruits - solved

---
+++ react_with_arrays/react-with-arrays_fruits/src/components/Card.jsx
@@ -0,0 +1,10 @@
+import "./Card.css";
+
+
+export default function Card({ name, color }) {
+  return (
+    <div className="card" style={{ backgroundColor: color }}>
+      {name}
+    </div>
+  );
+}
